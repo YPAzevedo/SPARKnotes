680e176a71ab4bab7374a7bf09b48a0622c927fa
YPAzevedo <yago.pereira.az@gmail.com>
2020-06-15 17:40:15 -0300

    💅🏻  update tile, icon and logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,13 @@
 import React, { memo } from "react";
 
+import logo from '../../assets/logo.svg'
+
 import { Container } from "./styles";
 
 const Header: React.FC = () => {
   return (
     <Container>
-      <div>
-        <span id="logo" role="img" aria-label="notebooks">
-          ⚡️📚
-        </span>
-        <span id="title">
-          <b>SPARK</b>notes
-        </span>
-      </div>
+      <img src={logo} alt="spark-notes" height={45}/>
     </Container>
   );
 };
